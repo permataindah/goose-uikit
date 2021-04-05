d8460945ed84e8417128fdc9fd565924c32326de
xclawkevin <34619818+xclawkevin@users.noreply.github.com>
2021-04-06 02:33:41 +0700

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2797,9 +2797,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#ff748c",
+    primaryBright: "#ff748c",
+    primaryDark: "#ff748c",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
